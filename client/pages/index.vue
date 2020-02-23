19efc53d897020c73ec54eb9e60eb31f183bde16
mirukushake <aricia@alicia-calhouns-imac-2.local>
2020-02-23 14:49:24 +0900

    uninit client folder

---
+++ client/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section">
+    <div class="columns is-multiline">
+      <div class="column is-one-fifth is-full-mobile">
+        Column
+      </div>
+      <div class="column is-one-fifth is-full-mobile">
+        Column
+      </div>
+      <div class="column is-one-fifth is-full-mobile">
+        Column
+      </div>
+      <div class="column is-one-fifth is-full-mobile">
+        Column
+      </div>
+      <div class="column is-one-fifth is-full-mobile">
+        Column
+      </div>
+      <div class="column is-one-fifth is-full-mobile">
+        Column
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'HomePage'
+}
+</script>
