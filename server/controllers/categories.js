b999f627a2cc3259d12b52aa64835dde1f473419
Alicia <hajimaruyo@gmail.com>
2020-03-03 05:21:07 +0000

    mess with api

--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -1,16 +1,20 @@
 const Category = require('../models/category');
 
+function getLocale (header) {
+	return header.split(',');
+}
+
 async function listCategories (ctx) {
   const language = ctx.query.locale || 1;
-  const subtitle = ctx.query.subtitle;
-  const categories = await Category.query().where('parent', null)
+	const subtitle = ctx.query.subtitle;
+	const categories = await Category.query().where('parent', null)
 	.modify('setLocale', 'category_names', 'cat_id', 'categories', language, subtitle)
-    .orderBy('order', 'name', 'id', 'identifier', 'parent', 'subtitle')
-    .withGraphFetched('children')
-    .modifyGraph('children', (builder) => {
-      builder.modify('setLocale', 'category_names', 'cat_id', 'categories', language, subtitle)
-        .orderBy('order', 'name', 'id', 'identifier', 'parent', 'subtitle');
-    });
+	.orderBy('order', 'name', 'id', 'identifier', 'parent', 'subtitle')
+	.withGraphFetched('children')
+	.modifyGraph('children', (builder) => {
+	builder.modify('setLocale', 'category_names', 'cat_id', 'categories', language, subtitle)
+	.orderBy('order', 'name', 'id', 'identifier', 'parent', 'subtitle');
+	});
   if (categories) {
     ctx.status = 200;
     ctx.body = {
